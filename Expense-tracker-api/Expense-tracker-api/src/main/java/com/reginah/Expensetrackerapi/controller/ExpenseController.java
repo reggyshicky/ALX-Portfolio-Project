3307e7bfcad66121116c55e844e21164a4786e64
reggyshicky <reginahshikanda@gmail.com>
2024-02-18 04:49:19 +0300

    retrieving one expense from the db

--- Expense-tracker-api/Expense-tracker-api/src/main/java/com/reginah/Expensetrackerapi/controller/ExpenseController.java
+++ Expense-tracker-api/Expense-tracker-api/src/main/java/com/reginah/Expensetrackerapi/controller/ExpenseController.java
@@ -4,6 +4,7 @@ import com.reginah.Expensetrackerapi.entity.Expense;
 import com.reginah.Expensetrackerapi.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,11 @@ public class ExpenseController {
     @GetMapping("/expenses")
     public List<Expense> getAllExpenses() {
         return expenseService.getAllExpenses();
+    }
+
+    @GetMapping("/expenses/{id}")
+    public String getExpenseById(@PathVariable("id") Long id) {
+        return "The expense id is " + id;
     }
 
 }
