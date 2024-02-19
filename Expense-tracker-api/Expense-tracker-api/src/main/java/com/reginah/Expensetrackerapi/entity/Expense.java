6ff4dcd897299ef35cbd6a9fc553fff25de8a21a
reggyshicky <reginahshikanda@gmail.com>
2024-02-20 02:49:22 +0300

    hibernate validator- validating all the other entity fields

--- Expense-tracker-api/Expense-tracker-api/src/main/java/com/reginah/Expensetrackerapi/entity/Expense.java
+++ Expense-tracker-api/Expense-tracker-api/src/main/java/com/reginah/Expensetrackerapi/entity/Expense.java
@@ -1,6 +1,7 @@
 package com.reginah.Expensetrackerapi.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -22,21 +23,28 @@ public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(name = "expense_name")
-    @NotNull(message = "Expense name must not be null")
+    @NotBlank(message = "Expense name must not be null")
     @Size(min=3, message = "Expense name must be atleast 3 characters")
     private String name;
 
     private String description;
+
     @Column(name = "expense_amount")
+    @NotNull(message = "Expense amount should not be null")
     private BigDecimal amount;
 
+    @NotBlank(message = "Category should not be null")
     private String category;
+
+    @NotNull(message = "Date must not be null")
     private Date date;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
     private Timestamp createdAt;
+
     @Column(name = "updated_at", nullable = false, updatable = false)
     @UpdateTimestamp
     private Timestamp updatedAt;
