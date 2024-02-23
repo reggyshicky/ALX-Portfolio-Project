c3f25a2d45f9c1d0f3cd6a2483dae540abbafaa4
reggyshicky <reginahshikanda@gmail.com>
2024-02-23 21:42:52 +0300

    configured multiple users - normal user and admin

--- Expense-tracker-api/Expense-tracker-api/src/main/java/com/reginah/Expensetrackerapi/config/SecurityConfig.java
+++ Expense-tracker-api/Expense-tracker-api/src/main/java/com/reginah/Expensetrackerapi/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.reginah.Expensetrackerapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -13,14 +20,78 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    PasswordEncoder passwordEncoder;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/login", "/register").permitAll()
-                                .requestMatchers("/api/v1/**")
-                                .authenticated()
+                                .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults()).build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("password")
+                        .roles("ADMIN", "USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
+
+
+
+
+
+
+//        @Bean
+////    public UserDetailsService userDetailsService(PasswordEncoder bCryptPasswordEncoder) {
+////        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+////        manager.createUser(User.withUsername("user")
+////                .password(bCryptPasswordEncoder.encode("userPass"))
+////                .roles("USER")
+////                .build());
+////        manager.createUser(User.withUsername("admin")
+////                .password(bCryptPasswordEncoder.encode("adminPass"))
+////                .roles("USER", "ADMIN")
+////                .build());
+////        return manager;
+////    }
+
+//        return http.csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(auth ->
+//                        auth.requestMatchers("/login", "/register").permitAll()
+//                                .requestMatchers("/api/v1/**")
+//                                .authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults()).build();
+
+
+
+
+
+
+
+
+
+
+
